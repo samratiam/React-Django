f541abb09528a5f0ce08950a15015f6e6bef5855
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-23 23:18:06 +0545

    URL for products is set and tested

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -5,4 +5,5 @@ from .views import home
 urlpatterns = [
     path('', home, name='api.home'),
     path('category/',include('api.category.urls')),
+    path('product/',include('api.product.urls')),
 ]
