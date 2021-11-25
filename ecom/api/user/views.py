8e032d9adc92217ea69aa5813fd5ff19c94a344a
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-25 23:35:55 +0545

    Signout user function added

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -56,3 +56,17 @@ def signin(request):
 
     except UserModel.DoesNotExist:
         return JsonResponse({'error': 'Invalid Email'})
+
+def signout(request, id):
+    logout(request) #this position can be changed inside this function
+
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        user.session_token = "0"
+        user.save()
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error': 'Invalid user ID'})
+    
+    return JsonResponse({'success': 'Logout success'})
