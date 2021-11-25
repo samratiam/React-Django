1aa7f2ac236f34ed55a5baeb0281e8e6867b07e0
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-25 22:08:03 +0545

    Custom user model created

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50, default='Anonymous')
+    email = models.EmailField(max_length=254, unique=True)
+
+    username = None
+
+    USERNAME_FIELD = 'email' #change usual username to email for sign up 
+    REQUIRED_FIELDS = []
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+
+    session_token = models.CharField(max_length=10, default=0) #default 0 for not logged in
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
