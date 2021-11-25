f11446596555106d56211b76e6ae1303e3016739
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-25 22:48:42 +0545

    Token is generated in user views

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+import random
+
+
+def generate_session_token(length = 10):
+    #you can choose another way to like using the list of values
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97,123)] + [str(i) for i in range(10)]) for _ in range(length))
