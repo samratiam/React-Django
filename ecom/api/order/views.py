193b8a88077270a64739cb64f899496063662bed
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-27 22:49:21 +0545

    User authentication for order added

--- ecom/api/order/views.py
+++ ecom/api/order/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from .serializers import OrderSerializer
+from .models import Order
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
+    
