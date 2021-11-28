2c6bc69ac691904826ae8e7a7d57c7ebd84eefdc
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-28 19:29:09 +0545

    Payment URLs setup and API tested

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.contib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
 
--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('product/',include('api.product.urls')),    
     path('user/',include('api.user.urls')),    
     path('order/',include('api.user.urls')),    
+    path('payment/',include('api.payment.urls')),    
 
     #to generate token automatically but we are using custom session
     path("/api-token-auth/", views.obtain_auth_token, name="api_token_auth")
