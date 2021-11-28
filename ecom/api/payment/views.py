b737ec9be60ffbc1b24626076a522f44026b9333
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-11-28 19:00:46 +0545

    Braintree token for user created

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.contib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
 
+import braintree
 # Create your views here.
+
+gateway = braintree.BraintreeGateway(
+  braintree.Configuration(
+    environment=braintree.Environment.Sandbox,
+    #api keys
+  )
+)
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
+
+@csrf_exempt
+def generate_token(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error':'Invalid session, Please login again'})
+    
+    return JsonResponse({'clientToken':gateway.client_token.generate(), 'success': True})
+
+
